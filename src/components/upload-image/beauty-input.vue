a29a07dbc833c8ac0247e0347e03419615e470ec
fuzongjian <2917066078@qq.com>
2018-01-02 18:44:47 +0800

    图片转base64完成。

---
+++ src/components/upload-image/beauty-input.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="input-wrapper">
+      <a href="" class="input"><input type="file" accept="image/*"></a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "beauty-input"
+    }
+</script>
+
+<style scoped>
+
+</style>
